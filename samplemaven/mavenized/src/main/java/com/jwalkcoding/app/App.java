8b35e1cd58d3956c0bc7746359fea8526ead56a1
Jay Walker <jwalker.nationwide@gmail.com>
2017-10-15 14:40:45 -0400

    mavenized project

---
+++ samplemaven/mavenized/src/main/java/com/jwalkcoding/app/App.java
@@ -0,0 +1,14 @@
+package com.jwalkcoding.app;
+
+/**
+ * Hello world!
+ *
+ */
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
